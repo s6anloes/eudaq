eab2f1aac2644b3e88940e3e1c0d37e31f14e4d1
s6anloes <a.loeschcke@gmail.com>
2022-07-26 21:28:40 +0200

    Improved Stop signal for producer

--- user/aidadualrocalo/misc/example_startrun.sh
+++ user/aidadualrocalo/misc/example_startrun.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 BINPATH=../../../bin
-$BINPATH/euCliRun &
+$BINPATH/euRun &
 #$BINPATH/euRun -n DualROCaloRunControl &
 sleep 1
-$BINPATH/euCliLog &
+$BINPATH/euLog &
 sleep 1
+#$BINPATH/euCliMonitor -n Ex0Monitor -t my_mon & 
 $BINPATH/euCliCollector -n DualROCaloDataCollector -t my_dc &
 $BINPATH/euCliProducer -n DualROCaloProducer -t my_pd0 &
