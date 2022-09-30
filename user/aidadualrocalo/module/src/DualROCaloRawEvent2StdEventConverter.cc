45b87477e4d0a314773e4de65178d2732ce9c055
s6anloes <a.loeschcke@gmail.com>
2022-09-30 16:02:58 +0100

    Commit before switching to older commit for debugging

--- user/aidadualrocalo/module/src/DualROCaloRawEvent2StdEventConverter.cc
+++ user/aidadualrocalo/module/src/DualROCaloRawEvent2StdEventConverter.cc
@@ -8,11 +8,10 @@
 struct drpixel{
   uint8_t board_id;
   uint8_t channel_id;
-  uint16_t lg_adc_value;
-  int16_t hg_adc_value;
+  int16_t adc_value;
 
   bool operator<(const drpixel &o) const{
-    return hg_adc_value < o.hg_adc_value; 
+    return adc_value < o.adc_value; 
   }
 };
 
@@ -22,8 +21,9 @@ public:
   bool Converting(eudaq::EventSPC d1, eudaq::StandardEventSP d2, eudaq::ConfigurationSPC conf) const override;
   std::vector<int> Filling_Channel_Map(eudaq::ConfigurationSPC conf) const;
   std::vector<int> Filling_Pedestals(eudaq::ConfigurationSPC conf) const;
-  std::priority_queue<drpixel> Filling_Priority_Queue(const std::vector<uint8_t> &data0, eudaq::ConfigurationSPC conf) const;
-  virtual eudaq::StandardPlane Filling_Plane(eudaq::StandardEventSP d2, std::priority_queue<drpixel> pq, std::vector<int> channel_map, uint8_t board_id) const;
+  //std::priority_queue<drpixel> Filling_Priority_Queue(const std::vector<uint8_t> &data0, eudaq::ConfigurationSPC conf) const;
+  //eudaq::StandardPlane Filling_Plane(eudaq::StandardEventSP d2, std::priority_queue<drpixel> pq, std::vector<int> channel_map, uint8_t board_id) const;
+  //eudaq::StandardPlane Filling_LowGain_Plane(eudaq::StandardEventSP d2, std::priority_queue<drpixel> pq, std::vector<int> channel_map, uint8_t board_id) const;
 
   static const uint32_t m_id_factory = eudaq::cstr2hash("DualROCaloEvent");
 };
@@ -78,7 +78,7 @@ std::vector<int> DualROCaloRawEvent2StdEventConverter::Filling_Pedestals(eudaq::
 
 }
 
-
+/*
 std::priority_queue<drpixel> DualROCaloRawEvent2StdEventConverter::Filling_Priority_Queue(const std::vector<uint8_t> &data0, eudaq::ConfigurationSPC conf) const{
   auto send_hg_value = conf->Get("send_hg_value", false);
 
@@ -110,8 +110,9 @@ std::priority_queue<drpixel> DualROCaloRawEvent2StdEventConverter::Filling_Prior
   }
 
   return pq;
-}
+}*/
 
+/*
 eudaq::StandardPlane DualROCaloRawEvent2StdEventConverter::Filling_Plane(eudaq::StandardEventSP d2, std::priority_queue<drpixel> pq, std::vector<int> channel_map, uint8_t board_id) const{
 
   // Identify the detetor type
@@ -124,22 +125,76 @@ eudaq::StandardPlane DualROCaloRawEvent2StdEventConverter::Filling_Plane(eudaq::
     uint8_t n = channel_map[pq.top().channel_id];
     uint16_t x = n % 16;
     uint16_t y = 19 - ((uint16_t) n/16 + 4*board_id); //channel numbering starts from the top (19 because we start from 0)
-    plane.PushPixel(x, y, pq.top().hg_adc_value);
+    plane.PushPixel(x, y, pq.top().adc_value);
     //std::cout<<"DualROCaloRAWEventConverter:: Pushing Pixel with hg_adc_value = " << std::to_string(pq.top().hg_adc_value) << std::endl;
     pq.pop();
   }
 
   return plane;
 
-}
+}*/
 
 bool DualROCaloRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq::StandardEventSP d2, eudaq::ConfigurationSPC conf) const{
+  
 
-  auto use_timestamps = conf->Get("use_timestamps", true);
+  std::cout << "Converting:: Getting event" << std::endl;
 
   auto ev = std::dynamic_pointer_cast<const eudaq::RawEvent>(d1);
   if(!ev)
     return false;
+
+  std::vector<int> channel_map = DualROCaloRawEvent2StdEventConverter::Filling_Channel_Map(conf);
+  std::vector<int> hg_pedestals = DualROCaloRawEvent2StdEventConverter::Filling_Pedestals(conf);
+  std::vector<int> lg_pedestals(64, 0);
+
+
+  std::cout << "Converting:: Filled pedestals and map" << std::endl;
+
+  std::priority_queue<drpixel> pq;
+  std::priority_queue<drpixel> lg_queue;
+
+  
+
+  auto &rawev = *ev;
+
+  const std::vector<uint8_t> &data0 = rawev.GetBlock(0);
+
+  uint8_t board_id = data0[0];
+  datait it0 = data0.begin()+1; // +1 because first entry in block is board_id
+
+  
+
+  while (it0 < data0.end()) {
+    
+    uint8_t channel_id = eudaq::getlittleendian<uint8_t>(&(*(it0)));
+    uint16_t lg_adc_value = eudaq::getlittleendian<uint16_t>(&(*(it0+2)));
+    uint16_t hg_adc_value = eudaq::getlittleendian<uint16_t>(&(*(it0+4)));
+
+    uint8_t n = channel_map[channel_id];
+    //int16_t ped_subtracted_hg = hg_adc_value - hg_pedestals[n+64*board_id];
+    //int16_t ped_subtracted_lg = lg_adc_value - lg_pedestals[n+64*board_id];
+    //std::cout << "PEDESTAL is = " << std::to_string(pedestals[n+64*board_id]) << std::endl;
+
+
+    //drpixel hg_pixel = {board_id, channel_id, ped_subtracted_hg};
+    //drpixel lg_pixel = {board_id, channel_id, ped_subtracted_lg};
+
+    drpixel hg_pixel = {board_id, channel_id, (int16_t)hg_adc_value};
+    //drpixel lg_pixel = {board_id, channel_id, lg_adc_value};
+
+    //pq.push(hg_pixel);
+    //lg_queue.push(lg_pixel);
+
+    it0 += 6;
+    
+  }
+
+  /*
+
+  std::cout << "Converting:: Looped over data block" << std::endl;
+  
+
+  auto use_timestamps = conf->Get("use_timestamps", true);
 
 
   if(!d2->IsFlagPacket()){
@@ -154,19 +209,51 @@ bool DualROCaloRawEvent2StdEventConverter::Converting(eudaq::EventSPC d1, eudaq:
       d2->SetTimestamp(d1->GetTimestampBegin()*1000000, d1->GetTimestampEnd()*1000000, d1->IsFlagTimestamp());
     }
   }
+  
+  // Identify the detetor type
+  d2->SetDetectorType("DualROCalo");
 
-  auto &rawev = *ev;
+  eudaq::StandardPlane plane(0, "DualROCalo", "DualROCalo");
+  plane.SetSizeZS(16, 20, 0);
 
-  const std::vector<uint8_t> &data0 = rawev.GetBlock(0);
-  uint8_t board_id = data0[0];
+  eudaq::StandardPlane plane_lg(1, "DualROCalo", "DualROCalo");
+  plane_lg.SetSizeZS(16, 20, 0);
 
-  std::vector<int> channel_map = DualROCaloRawEvent2StdEventConverter::Filling_Channel_Map(conf);
-  std::vector<int> pedestals = DualROCaloRawEvent2StdEventConverter::Filling_Pedestals(conf);
-  std::priority_queue<drpixel> pq = DualROCaloRawEvent2StdEventConverter::Filling_Priority_Queue(data0, conf);
-  eudaq::StandardPlane plane = DualROCaloRawEvent2StdEventConverter::Filling_Plane(d2, pq, channel_map, board_id);
+
+  std::cout << "Converting:: Creating planes" << std::endl;
+
+  for (int p=0; p<1; p++){
+    uint8_t n = channel_map[pq.top().channel_id];
+    uint16_t x = n % 16;
+    uint16_t y = 19 - ((uint16_t) n/16 + 4*board_id); //channel numbering starts from the top (19 because we start from 0)
+    plane.PushPixel(x, y, pq.top().adc_value);
+    //std::cout<<"DualROCaloRAWEventConverter:: Pushing Pixel with hg_adc_value = " << std::to_string(pq.top().hg_adc_value) << std::endl;
+    pq.pop();
+  }
+
+
+  std::cout << "Converting:: Looped over pq" << std::endl;
+
+  for (int k=0; k<1; k++){
+    uint8_t n = channel_map[lg_queue.top().channel_id];
+    uint16_t x = n % 16;
+    uint16_t y = 19 - ((uint16_t) n/16 + 4*board_id); //channel numbering starts from the top (19 because we start from 0)
+    plane_lg.PushPixel(x, y, lg_queue.top().adc_value);
+    //std::cout<<"DualROCaloRAWEventConverter:: Pushing Pixel with hg_adc_value = " << std::to_string(pq.top().hg_adc_value) << std::endl;
+    lg_queue.pop();
+  }
+
+
+
+  std::cout << "Converting:: Looped over lg_queue" << std::endl;
   
   d2->AddPlane(plane);
+  
+  std::cout << "Converting:: Added plane 0" << std::endl;
+  d2->AddPlane(plane_lg);
+  std::cout << "Converting:: Added plane 1" << std::endl;
   //eudaq::mSleep(1000);
+  */
 
   return true;
 }
